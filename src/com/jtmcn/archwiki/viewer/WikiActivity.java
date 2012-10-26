c8642ec363ef44d27325ba653f45261045118104
Joel McNierney <joel.mcnierney@gmail.com>
2012-10-25 18:00:23 -0700

    Exit button, Preferences added

--- src/com/jtmcn/archwiki/viewer/WikiActivity.java
+++ src/com/jtmcn/archwiki/viewer/WikiActivity.java
@@ -3,7 +3,12 @@ package com.jtmcn.archwiki.viewer;
 import android.app.Activity;
 import android.app.SearchManager;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.webkit.WebSettings;
@@ -20,10 +25,12 @@ public class WikiActivity extends Activity implements OnClickListener {
 	String titleString;
 	ProgressBar progressBar;
 	Button searchButton;
+	Button overflowButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
 		Intent intent = getIntent();
 
 		setContentView(R.layout.wiki_layout);
@@ -46,6 +53,10 @@ public class WikiActivity extends Activity implements OnClickListener {
 		progressBar = (ProgressBar) findViewById(R.id.ProgressBar);
 		searchButton = (Button) findViewById(R.id.search);
 		searchButton.setOnClickListener(this);
+
+		overflowButton = (Button) findViewById(R.id.overflow);
+		overflowButton.setOnClickListener(this);
+
 		tvTitle = (TextView) findViewById(R.id.title);
 
 		WikiChromeClient myChrome = new WikiChromeClient(progressBar, tvTitle);
@@ -54,11 +65,65 @@ public class WikiActivity extends Activity implements OnClickListener {
 
 	public void setWebSettings() {
 		WebSettings webSettings = wikiViewer.getSettings();
-		webSettings.setBuiltInZoomControls(true);
+		// webSettings.setBuiltInZoomControls(true);
+
+		PreferenceManager.setDefaultValues(getApplicationContext(),
+				R.xml.settings, false);
+		SharedPreferences prefs = PreferenceManager
+				.getDefaultSharedPreferences(getApplicationContext());
+		String fontSize = prefs.getString("listPref", "Normal");
+		// Toast.makeText(this, "fontSize: " + fontSize,
+		// Toast.LENGTH_LONG).show();
+
+		int fontDpi = Integer.parseInt(fontSize);
+
+		webSettings.setDefaultFontSize(fontDpi);
 	}
 
 	public void onClick(View v) {
-		onSearchRequested();
+		switch (v.getId()) {
+		case R.id.search:
+			onSearchRequested();
+			break;
+		case R.id.overflow:
+			// Toast.makeText(this, "Overflow", Toast.LENGTH_LONG).show();
+			openOptionsMenu();
+			break;
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.options, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.menu_settings:
+			// Toast.makeText(this, "Settings", Toast.LENGTH_LONG).show();
+			Intent p = new Intent("com.jtmcn.archwiki.viewer.WIKIPREFS");
+			startActivityForResult(p, 0);
+			break;
+		case R.id.exit:
+			// Toast.makeText(this, "Exit!", Toast.LENGTH_LONG).show();
+			// tvTitle = null;
+			this.finish();
+			break;
+		}
+		return true;
+	}
+
+	/*
+	 * Activity needs to be reloaded if the font size is changed. This isn't
+	 * ideal because it also reloads when nothing changes.
+	 */
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		setWebSettings();
 	}
 
 }
